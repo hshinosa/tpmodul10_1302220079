7b90aa5864011b432819c3bad40367c199a520b0
ksHl <hashfih12@gmail.com>
2024-04-29 01:15:15 +0700

    menambahkan API Mahasiswa

---
+++ tpmodul10_1302220079/Controllers/MahasiswaController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tpmodul10_1302220079;
+
+public class MahasiswaController : Controller
+{
+    // GET /api/mahasiswa: Returns a list of all students
+    [HttpGet]
+    [Route("/api/mahasiswa")]
+    public IActionResult GetAllStudents()
+    {
+        return Ok(StudentData.Students);
+    }
+
+    // GET /api/mahasiswa/{index}: Returns the student object at the specified index
+    [HttpGet]
+    [Route("/api/mahasiswa/{index}")]
+    public IActionResult GetStudentByIndex(int index)
+    {
+        if (index < 0 || index >= StudentData.Students.Count)
+        {
+            return NotFound();
+        }
+
+        return Ok(StudentData.Students[index]);
+    }
+
+    // POST /api/mahasiswa: Adds a new student object with provided name and ID
+    [HttpPost]
+    [Route("/api/mahasiswa")]
+    public IActionResult AddStudent([FromBody] Mahasiswa newStudent)
+    {
+        if (newStudent == null)
+        {
+            return BadRequest();
+        }
+
+        StudentData.Students.Add(newStudent);
+        return Ok();
+    }
+
+    // DELETE /api/mahasiswa/{index}: Deletes the student object at the specified index
+    [HttpDelete]
+    [Route("/api/mahasiswa/{index}")]
+    public IActionResult DeleteStudent(int index)
+    {
+        if (index < 0 || index >= StudentData.Students.Count)
+        {
+            return NotFound();
+        }
+
+        StudentData.Students.RemoveAt(index);
+        return Ok();
+    }
+}
---
+++ tpmodul10_1302220079/Mahasiswa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tpmodul10_1302220079;
+public class Mahasiswa
+{
+    public string Nama { get; set; }
+    public string Nim { get; set; }
+
+    public Mahasiswa(string nama, string nim)
+    {
+        Nama = nama;
+        Nim = nim;
+    }
+}
+
+public static class StudentData
+{
+    public static List<Mahasiswa> Students { get; } = new List<Mahasiswa>()
+    {
+        new Mahasiswa("⁠Muhammad Hashfi Hadyan", "1302220079"),
+        new Mahasiswa("Muhammad Daffa Fadillah", "1302223124"),
+        new Mahasiswa("Muhammad Fauzan Majid", "1302220144"),
+        new Mahasiswa("Arinza Aurelvia", "1302220022"),
+        new Mahasiswa("Soraya Haidar Salma", "1302223006"),
+        new Mahasiswa("Maulana Farras", "103022390001")
+    };
+}
